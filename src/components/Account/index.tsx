513a717d8197bfd65550db125efaa8ee992835f4
danieljesus <daniel.j.rodrigues06@gmail.com>
2020-07-22 15:54:24 -0300

    add UpdateUser modal

--- src/components/Account/index.tsx
+++ src/components/Account/index.tsx
@@ -1,7 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useContext, useCallback, useState } from 'react';
 
 import { MdExitToApp, MdEdit } from 'react-icons/md';
 
+import UserModal from './accountModal';
 import { AuthContext } from '../../context/AuthContext';
 
 import {
@@ -12,23 +13,40 @@ import {
 
 const Account: React.FC = () => {
   const { user, logout } = useContext(AuthContext);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = useCallback(() => {
+    setIsModalOpen(true);
+  }, []);
+
+  const handleCloseModal = useCallback(() => {
+    setIsModalOpen(false);
+  }, []);
 
   return (
-    <Container>
-      <Content>
-        <AvatarArea>
-          <img 
-            src="https://api.adorable.io/avatars/195/abott@adorable.png" 
-            alt="avatar"
-          />
-          <h3>{user.name}</h3>
-          <MdEdit size={21} color="#4D4646" />
-        </AvatarArea>
-        <button onClick={logout} style={{ border: 'none', backgroundColor: 'transparent' }} >
-          <MdExitToApp size={45} color="#4D4646" />
-        </button>
-      </Content>
-    </Container>
+    <>
+      <Container>
+        <Content>
+          <AvatarArea>
+            <img 
+              src="https://api.adorable.io/avatars/195/abott@adorable.png" 
+              alt="avatar"
+            />
+            <h3>{user.name}</h3>
+            <button onClick={handleOpenModal} style={{ border: 'none', backgroundColor: 'transparent' }} >
+              <MdEdit size={21} color="#4D4646" />
+            </button>
+          </AvatarArea>
+          <button onClick={logout} style={{ border: 'none', backgroundColor: 'transparent' }} >
+            <MdExitToApp size={45} color="#4D4646" />
+          </button>
+        </Content>
+      </Container>
+      <UserModal 
+        isModalOpen={isModalOpen}
+        handleCloseModal={handleCloseModal}
+      />
+    </>
   );
 }
 
